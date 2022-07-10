148bbe167f106d64404ab87aeb54b7e6e7942fd3
Corentin Dupont <corentin.dupont@gmail.com>
2022-07-10 17:03:50 +0200

    wip

--- lora/TTN_adapter/src/index.js
+++ lora/TTN_adapter/src/index.js
@@ -2,16 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
+const mqtt = require('mqtt')
 
-const port = 8000;
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const options = {'username': 'mydev@ttn',
+                 'password': 'NNSXS.ZREHHZZYMBUFCENADJTYOP5H3NXHMMZ6XRQ7DLI.ENEXKPLQIZ6U2G64ENXHJOJVQST6OSFSUMY4ZAM5NL3GM3XBOP3A'}
+const client  = mqtt.connect('mqtt://eu1.cloud.thethings.network', options)
 
-app.post('/', async (req, res) => {
-  const body = req.body;
+client.on('connect', function () {
+  client.subscribe('v3/mydev@ttn/devices/eui-a8610a33373a6a0c/up', function (err) {
+    console.log("subscribe res: " + err)
+  })
+})
+
+client.on('message', function (topic, message) {
+  sendSpotAir(JSON.parse(message.toString()))
+  client.end()
+})
+
+
+async function sendSpotAir(body) {
     
+  console.log(JSON.stringify(body))
   let wind_speed     = body.uplink_message.decoded_payload.analog_in_1
   let wind_direction = body.uplink_message.decoded_payload.analog_in_2
   let timestamp = Math.floor(new Date().getTime() / 1000) //body.uplink_message.rx_metadata.timestamp
@@ -22,9 +33,8 @@ app.post('/', async (req, res) => {
              "direction": wind_direction,
              "vmoy": wind_speed}
 
+  console.log("posting value to spotair: " + JSON.stringify(val))
   const res2 = await axios.post('https://intdata.spotair.mobi/balises/releve-put.php', val);
 
-  res.send(res2.data);
-});
+}
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
